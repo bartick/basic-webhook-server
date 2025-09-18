cda8e949aacef1ddd73562bea1c4655572305253
bartick <bartickmaiti1998@gmail.com>
2025-09-18 14:41:12 +0530

    fix: json parser

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Sample route
