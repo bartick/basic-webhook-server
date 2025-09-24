88029af20d15df35fa9f287a3072ccc38ad1b669
bartick <bartickmaiti1998@gmail.com>
2025-09-24 10:59:17 +0530

    define axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
