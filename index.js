8af3a7f46a28030b1b4ca99f0b52c4a2e8029cac
bartick <bartickmaiti1998@gmail.com>
2025-09-24 10:58:23 +0530

    add: request to my backend

--- index.js
+++ index.js
@@ -29,8 +29,12 @@ app.listen(PORT, () => {
 });
 
 app.get('/slack', (req, res) => {
-    console.log('Header:', JSON.stringify(req.headers, null, 2));
-    console.log('Slack Webhook received:', JSON.stringify(req.body, null, 2));
-    console.log('Query Params:', JSON.stringify(req.query, null, 2));
-    res.status(200).send('Slack Webhook received');
+    axios.get(`http://localhost:8010/anonymous/v1/slack/auth?code=${req.query.code}&state=${req.query.state}`)
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching Slack auth:', error);
+            res.status(500).send('Error fetching Slack auth');
+        });
 });
